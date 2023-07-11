893120a469699d14916ddb012f2b7612e2966ac0
markdave123-py <nwaekwudavid@gmail.com>
2023-07-11 17:58:52 +0100

    Deposit tests

--- appts/app/test/deposit.test.ts
+++ appts/app/test/deposit.test.ts
@@ -8,14 +8,44 @@ describe('Deposit Tests', () =>{
     let token;
     let postUrl;
     let getUrl;
-    let inputAmount;
+    let inputBody;
 
     beforeEach(() =>{
         //id fetched from  the database
         token = genToken('5673gdu33hhs')  // dummy id used
-        inputAmount = { amount: 67000};
+        inputBody = { amount: 67000, currency: 'usd   '};
 
     });
+
+    const makePostRequest = (wallet_id: any) => {
+        postUrl = `/wallet/${wallet_id}/deposits`
+        return request(app)
+        .post(postUrl)
+        .set('x-auth-token', token!)
+        .send(inputBody!);
+
+    }
+
+
+    const makeGetRequest = (wallet_id: any) => {
+        getUrl = `/wallet/${wallet_id}/deposits`
+
+        return request(app)
+        .get(getUrl)
+        .set('x-auth-token', token!);
+
+    }
+
+    it('should return 400 error if no deposit amount or currency is given', async () => {
+
+        inputBody!.amount = undefined
+        inputBody!.currency = undefined
+
+        const response = await makePostRequest('mPo0qpV3sM82s56k')
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe('{"message":"Provide the currency and amount to be deposited"}')
+
+    })
 
     
 })
